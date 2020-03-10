84da2e219c61b6eb4f7ef394dd4c2ca70f0e586d
Shikha25 <56836929+shiks25@users.noreply.github.com>
2020-03-10 21:16:07 +0530

    Create dfs_cycle.cpp

---
+++ dfs_cycle.cpp
@@ -0,0 +1,61 @@
+//Using DFS, find if there are cycles in a given graph.
+#include<iostream>
+#include<set>
+using namespace std;
+int arr[30][30];
+bool dfs(int vertex, set<int>&visited, int parent,int n) {
+   visited.insert(vertex);
+   for(int v = 0; v<n; v++) {
+      if(arr[vertex][v]) {
+         if(v == parent)    //if v is the parent not move that direction
+            continue;
+         if(visited.find(v) != visited.end())    //if v is already visited
+            return true;
+         if(dfs(v, visited, vertex,n))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool hasCycle(int n) {
+   set<int>visited;       //visited set
+   for(int v = 0; v<n; v++) {
+      if(visited.find(v) != visited.end())    //when visited holds v, jump to next iteration
+         continue;
+      if(dfs(v, visited, -1,n)) {    //-1 as no parent of starting vertex
+         return true;
+      }
+   }
+   return false;
+}
+
+int main() {
+   bool res;
+  int n;
+   cout<<"enter the number of vertices\n";
+   cin>> n;
+   cout<<"enter the adjacency matrix\n";
+   for(int i=0;i<n;i++){
+   for(int j=0;j<n;j++){
+   cin>>arr[i][j];
+   }
+   }
+   res = hasCycle(n);
+   if(res)
+      cout << "The graph has cycle." << endl;
+   else
+      cout << "The graph has no cycle." << endl;
+    return 0;
+   }
+   /*output
+enter the number of vertices                                                                                                  
+5                                                                                                                             
+enter the adjacency matrix                                                                                                    
+0 1 0 0 0                                                                                                                     
+1 0 1 1 0                                                                                                                     
+0 1 0 0 1  
+0 1 0 0 1
+0 0 1 1 0
+The graph has cycle.   
+*/
