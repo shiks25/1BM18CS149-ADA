10a5b7b3ef45e0fab80089d1d1c12ec98502807c
Shikha25 <56836929+shiks25@users.noreply.github.com>
2020-03-16 18:30:04 +0530

    Add files via upload

---
+++ lexo_topo.cpp
@@ -0,0 +1,67 @@
+/*Given a directed graph with N vertices and M edges that may contain cycles, the task is to find the lexicographically
+smallest topological ordering of the graph if it exists otherwise print -1 (if the graph has cycles).*/
+#include<bits/stdc++.h>
+using namespace std;
+stack<int> s;
+int n,a[30][30],q[30],f=0,r=-1;
+void lexotopo()
+{
+	vector<int> in_deg(n,0);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(a[i][j]==1)
+				in_deg[j]++;
+		}
+
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		if(in_deg[i]==0)
+			q[++r]=i;
+	}
+	int c=0;
+	vector<int> toporder;
+	while(f<=r)
+	{
+		int min=q[f++];
+
+		toporder.push_back(min);
+		for(int i=0;i<n;i++)
+		{
+			if(a[min][i]==1)
+			{
+				in_deg[i]--;
+				if(in_deg[i]==0)
+					q[++r]=i;
+			}
+
+		}
+		c++;
+	}
+	if(c!=n)
+	{
+		cout<<-1<<endl;
+		return;
+	}
+	for(int i=0;i<toporder.size();i++)
+		cout<<toporder[i]<<" ";
+
+}
+
+int main()
+{
+	cout<<"Enter the no of vertices"<<endl;
+	cin>>n;
+	cout<<"Enter the adjacency matrix"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+			cin>>a[i][j];
+	}
+	cout<<"lexicographically smallest topological ordering is"<<endl;
+	lexotopo();
+	return 0;
+}
